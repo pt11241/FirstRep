b07049c2be047f0dbac5aeaaf0805f3eb609b821
pt11241 <html23232@gmail.com>
2023-09-08 16:18:10 +0300

    qgdfg

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 void main(){
+    int e;
+    int c;
+    
+    
+    scanf("%d %d",&e, &c);
+    int sum = e + c; 
+    printf("value:%d \n", sum);
     printf("Hello World\n");
+    
 }
